0fe5b145c06e3abd54ae902359bc3310c191ff23
Maciej Pieprzyk <maciej.pieprzyk@computec.pl>
2024-11-23 12:30:05 +0100

    save vehicle changes

--- ae-3.0/CT.VehOne/Source/CT.VehOne/www/src/controller/VehicleDetails.controller.ts
+++ ae-3.0/CT.VehOne/Source/CT.VehOne/www/src/controller/VehicleDetails.controller.ts
@@ -9,6 +9,7 @@ import ODataHelper from "computec/appengine/uicore/helpers/ODataHelper";
 import VehicleMasterData from "../models/VehicleMasterData";
 import ErrorHelper from "computec/appengine/uicore/helpers/ErrorHelper";
 import MessageBox from "sap/m/MessageBox";
+import ODataModel from "sap/ui/model/odata/v4/ODataModel";
 export default class VehicleDetails extends BaseController<VehicleDetailsViewModel> {
 	private VEHICLE_UPDATE_GROUP_ID = "vehicleUpdateGroup";
 	onInit(): void {
@@ -34,7 +35,11 @@ export default class VehicleDetails extends BaseController<VehicleDetailsViewMod
 	}
 	private _getViewBindingContext = () =>
 		this.getView().getBindingContext(Component.ODATA_MODEL_NAME) as Context;
-	private _getCurrentVehicle = () => ODataHelper.getObject<VehicleMasterData>(this.getView(), Component.ODATA_MODEL_NAME);
+	private _getCurrentVehicle = () =>
+		ODataHelper.getObject<VehicleMasterData>(this.getView(), Component.ODATA_MODEL_NAME);
+	private _getViewVehicleModel = () =>
+		this.getView().getModel(Component.ODATA_MODEL_NAME) as ODataModel;
+	private _refresh = () => this._getViewVehicleModel().refresh();
 	//#endregion VIEW
 
 	//#region EVENT HANDLERS
@@ -47,20 +52,57 @@ export default class VehicleDetails extends BaseController<VehicleDetailsViewMod
 		if (!deleteConfirmation) return;
 		try {
 			await this._deleteVehicle();
-			this.navTo("home", {
-				refresh: true,
-			});
+			this.navTo(
+				"home",
+				{
+					refresh: true,
+				},
+				false
+			);
 		} catch (error) {
 			const err = ErrorHelper.getError(error);
 			MessageBox.error(this.translate("vehicleDeleteFailure", [err]));
 		}
 	}
+	onEditPressed = () => (this.getViewModel().editMode = true);
 
+	async onCancelPressed() {
+		if (this._getViewBindingContext().hasPendingChanges()) {
+			const cancelConfirmation = await MessageHelper.simpleConfirm(
+				this.translate("vehicleCancelConfirmation"),
+				this.translate("cancel")
+			);
+			if (!cancelConfirmation) return;
+			this.getViewModel().busy = true;
+			await this._getViewBindingContext().resetChanges();
+		}
+		this.getViewModel().editMode = false;
+		this.getViewModel().busy = false;
+	}
+	async onSavePressed() {
+		this.getViewModel().busy = true;
+		try {
+			if (this._getViewBindingContext().hasPendingChanges()) {
+				await this._saveVehicle();
+				if (this._getViewBindingContext().hasPendingChanges())
+					throw new Error(this.translate("vehicleSaveFailure", ["Pending changes remains"]));
+			}
+			this.getViewModel().editMode = false;
+			this._refresh();
+		} catch (error) {
+			const err = ErrorHelper.getError(error);
+			MessageBox.error(this.translate("vehicleSaveFailure", [err]));
+		} finally {
+			this.getViewModel().busy = false;
+		}
+	}
 	//#endregion EVENT HANDLERS
 
 	//#region PRIVATE METHODS
-	private async _deleteVehicle() {
-		await this._getViewBindingContext().delete("$auto");
-	}
+	private _deleteVehicle = () => this._getViewBindingContext().delete("$auto");
+
+	private _saveVehicle = () =>
+		this._getViewVehicleModel().submitBatch(this.VEHICLE_UPDATE_GROUP_ID);
+
 	//#endregion PRIVATE METHODS
 }
