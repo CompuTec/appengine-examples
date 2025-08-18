78c9f46cd63e8ab5ab7c59ed19951659f64a7ab2
Maciej Pieprzyk <maciej.pieprzyk@computec.pl>
2025-08-18 13:32:54 +0200

    Fixed namespaces

--- ae-3.0/CT.VehOne/Source/CT.VehOne/www/src/controller/App.controller.ts
+++ ae-3.0/CT.VehOne/Source/CT.VehOne/www/src/controller/App.controller.ts
@@ -3,7 +3,6 @@ import AEBaseController from "computec/appengine/uicore/plugin/BaseController";
 /**
  * @namespace ct.vehone.controller
  */
-
 export default class App extends AEBaseController {
 	onInit(): void {
 		super.onInit();
