0bfaa98246009ba42c8bc869be1c4c2a31eba636
michalw <michal.wieczorek@computec.pl>
2025-09-01 16:59:53 +0200

    Refactor UDO table creation using builder pattern for improved readability and maintainability

--- ae-3.0/CT.VehOne/Source/CT.VehOne.BL/DatabaseSetup/Beans/Document/DocumentSubLines1Table.cs
+++ ae-3.0/CT.VehOne/Source/CT.VehOne.BL/DatabaseSetup/Beans/Document/DocumentSubLines1Table.cs
@@ -15,74 +15,17 @@ public class DocumentSubLines1Table : UDOManager
 
     protected override IUDOTable CreateUDOTable()
     {
-        List<IUDOField> fields = CreateFieldsForTable();
-        IUDOTable UdoTable = new UDOTable(fields, ROW_TABLE_BOM_ITEMS_NAME, ROW_TABLE_BOM_ITEMS_DESCRIPTION,
-            BoUTBTableType.bott_DocumentLines);
-        UdoTable.RegisteredUDOName = ROW_TABLE_BOM_ITEMS_NAME;
-        UdoTable.RegisteredUDOCode = ROW_TABLE_BOM_ITEMS_DEFAULT_CODE;
+        var builder = UDOTable.CreateDocumentLinesBuilder()
+            .WithTable(ROW_TABLE_BOM_ITEMS_NAME, ROW_TABLE_BOM_ITEMS_DESCRIPTION)
+            .WithArchiveTable("CT_VO_ADOC3")
+            .AddField("Sequence", "Sequence").AsNumeric().Add()
+            .AddField("ItemCode", "ItemCode").AsAlpha(50).Add()
+            .AddField("Description", "Item Description").AsAlpha(200).Add()
+            .AddField("Intermediat", "Intermediat").AsAlpha(20).Add()
+            .AddField("Parent", "Parent").AsNumeric().Add()
+            .AddField("Quantity", "Quantity").AsQuantity().Add();
 
-        UdoTable.CanArchive = BoYesNoEnum.tYES;
-        UdoTable.ArchiveTableName = "CT_VO_ADOC3";
-
-        return UdoTable;
-    }
-
-    protected List<IUDOField> CreateFieldsForTable()
-    {
-        List<IUDOField> fields = new List<IUDOField>();
-
-        IUDOField Sequence = new UDOTableField();
-        Sequence.SetName("Sequence");
-        Sequence.SetDescription("Sequence");
-        Sequence.SetType(BoFieldTypes.db_Numeric);
-        Sequence.SetEditSize(10);
-        //Sequence.SetDefaultValue("0");
-        fields.Add(Sequence);
-
-        IUDOField ItemCode = new UDOTableField();
-        ItemCode.SetName("ItemCode");
-        ItemCode.SetDescription("ItemCode");
-        ItemCode.SetType(BoFieldTypes.db_Alpha);
-        ItemCode.SetEditSize(50);
-        //ItemCode.SetDefaultValue("0");
-        fields.Add(ItemCode);
-
-        IUDOField Description = new UDOTableField();
-        Description.SetName("Description");
-        Description.SetDescription("Item Description");
-        Description.SetType(BoFieldTypes.db_Alpha);
-        Description.SetEditSize(200);
-        //Description.SetDefaultValue("0");
-        fields.Add(Description);
-
-        IUDOField Intermediat = new UDOTableField();
-        Intermediat.SetName("Intermediat");
-        Intermediat.SetDescription("Intermediat");
-        Intermediat.SetType(BoFieldTypes.db_Alpha);
-        Intermediat.SetEditSize(20);
-        //Revision.SetDefaultValue("0");
-        fields.Add(Intermediat);
-
-
-        IUDOField Parent = new UDOTableField();
-        Parent.SetName("Parent");
-        Parent.SetDescription("Parent");
-        Parent.SetType(BoFieldTypes.db_Numeric);
-        Parent.SetEditSize(10);
-        //Factor.SetDefaultValue("0");
-        fields.Add(Parent);
-
-        IUDOField Quantity = new UDOTableField();
-        Quantity.SetName("Quantity");
-        Quantity.SetDescription("Quantity");
-        Quantity.SetType(BoFieldTypes.db_Float);
-        Quantity.SetSubType(BoFldSubTypes.st_Quantity);
-        Quantity.SetEditSize(20);
-        //Quantity.SetDefaultValue("0");
-        fields.Add(Quantity);
-
-
-        return fields;
+        return builder.Build();
     }
 
     protected override void SetChildTables()
